893f5ad3f9c3da938b08d545b0e4fda02b1c6933
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-01-16 06:20:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Jarbi Bonilla"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Stafford, VA"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is Somebody Else by The 1975"
+  end 
 end
