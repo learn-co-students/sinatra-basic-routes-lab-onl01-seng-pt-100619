3780ee8c5b5fbb0a1688d968322b6aba298227ad
Jeff Swisher <jefftswisher@gmail.com>
2020-01-24 02:47:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end 
+  
+  get '/name' do 
+    "My name is Jeff"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Scottsdale"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is \'Life is good\'"
+  end 
+  
+  
 end
